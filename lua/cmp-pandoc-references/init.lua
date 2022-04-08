aace1eb38d5c6e063e6d628117820fdd3fd05a6b
Micah Halter <micah@balena.io>
2022-04-08 13:50:03 -0400

    Add rmarkdown
    Signed-off-by: Micah Halter <micah@balena.io>

--- lua/cmp-pandoc-references/init.lua
+++ lua/cmp-pandoc-references/init.lua
@@ -1,27 +1,27 @@
 local source = {}
-local refs = require 'cmp-pandoc-references.references'
+local refs = require("cmp-pandoc-references.references")
 
 source.new = function()
-	return setmetatable({}, {__index = source})
+	return setmetatable({}, { __index = source })
 end
 
 -- Add another filetype if needed
 source.is_available = function()
-	return vim.o.filetype == 'pandoc' or vim.o.filetype == 'markdown'
+	return vim.o.filetype == "pandoc" or vim.o.filetype == "markdown" or vim.o.filetype == "rmd"
 end
 
 source.get_keyword_pattern = function()
-	return '[@][^[:blank:]]*'
+	return "[@][^[:blank:]]*"
 end
 
 source.complete = function(self, request, callback)
-  local lines = vim.api.nvim_buf_get_lines(self.bufnr or 0, 0, -1, false)
-  local entries = refs.get_entries(lines)
+	local lines = vim.api.nvim_buf_get_lines(self.bufnr or 0, 0, -1, false)
+	local entries = refs.get_entries(lines)
 
-  if entries then
-    self.items = entries
-    callback(self.items)
-  end
+	if entries then
+		self.items = entries
+		callback(self.items)
+	end
 end
 
 return source
